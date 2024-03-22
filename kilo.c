7c571b0509d0fa35a5724d40854392497a86057f
Gabriel das Neves <gneves@Gabriels-MacBook-Air.local>
2024-03-22 17:53:34 -0300

    First commit

---
+++ kilo.c
@@ -0,0 +1,13 @@
+/*
+ * This code is entirely based on https://viewsourcecode.org/snaptoken/kilo/index.html
+ * I do not intend to reclaim ownership or intelectual property over the following lines.
+ * I'm just doing this to improve my programming knowledge.
+*/
+#include <stdio.h>
+
+
+int main(void) {
+    printf("Hello, World!\n");
+
+    return 0;
+}
